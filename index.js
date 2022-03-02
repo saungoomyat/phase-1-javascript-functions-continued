d1ceec273ab4ce80d542e42f8f0297a74c726def
root <saung.myat@gmail.com>
2022-03-01 22:06:52 -0500

    commit tree 1deec2bbd115c0b91f60a276e84027eb07e02dc6

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun (activity = 'roller-skate') {   
+
+    return 'This Saturday, I want to ' + activity + '!';
+}
+
+function mondayWork (chore = 'go to the office') {
+
+    return 'This Monday, I will ' + chore + '.';
+}
+
+function wrapAdjective (flair) {
+        return function result (compliment = 'special') {
+            return 'You are ' + flair + compliment + flair +'!'; 
+        }
+        
+}
